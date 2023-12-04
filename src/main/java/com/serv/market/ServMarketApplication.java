9d84e2c71f437b45fa830206c75f9730eefaf468
Efrain Giron <efrainbond@hotmail.com>
2023-12-04 03:36:58 -0600

    Creado un hola mundo.

---
+++ src/main/java/com/serv/market/ServMarketApplication.java
@@ -0,0 +1,13 @@
+package com.serv.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serv/market/ServMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serv.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
